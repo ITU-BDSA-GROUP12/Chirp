9cb4d952ebb711622059abcb658f54a9aa16908f
Allan <asia@itu.dk>
2023-08-31 15:58:32 +0200

    Added read function

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,5 +1,34 @@
-﻿foreach (var arg in args)
-    Console.WriteLine(arg);
+﻿using System.IO;
+using System;
+
+if (args[0]=="read")
+{   // Read part from: https://learn.microsoft.com/en-us/dotnet/standard/io/how-to-read-text-from-a-file
+    try
+        {
+            // Open the text file using a stream reader.
+            using (var sr = new StreamReader(@"..\src\chirp_cli_db.csv"))
+            {
+                // Read the stream as a string, and write the string to the console.
+                Console.WriteLine(sr.ReadToEnd());
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+
+        //Console.WriteLine(DateTimeOffset())
+
+
+}
+
+if (args[0]=="write")
+{
+    //WRITE CHEEP
+}
+
+
 
 // using System.Threading.Tasks.Dataflow;
 
