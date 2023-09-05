94883a78fd151df0cf104deffed6b4b79a12d19c
Allan <asia@itu.dk>
2023-09-05 11:29:44 +0200

    Finished session 1 Longer description...
    We managed to r/w in the csv file as described in the README_PROJECT.md
    
    Co-authored-by: Lukas <lupa@itu.dk>
    Markus <mbrh@itu.dk>
    Rasmus <rhra@itu.dk>
    Co-pilot
    chat.openai.com

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,60 +1,54 @@
 ﻿using System.IO;
 using System;
 
-var shouldRun = true;
+//All references to "GPT" in comments are references to chat.opanai.com
 
-while (shouldRun)
-{
-
-
-}
+string path = "../src/chirp_cli_db.csv"; //Path to CSV file 
 
 if (args[0]=="read")
 {   // Read part from: https://learn.microsoft.com/en-us/dotnet/standard/io/how-to-read-text-from-a-file
     try
         {
-            // Open the text file using a stream reader.
-            using (var sr = new StreamReader(@"..\src\chirp_cli_db.csv"))
+        // Open the text file using a stream reader.
+        using var sr = new StreamReader(path);
+        
+        
+        var line = sr.ReadLine();       //Read first line without printing
+
+        //Using https://stackoverflow.com/questions/5282999/reading-csv-file-and-storing-values-into-an-array
+        while (!sr.EndOfStream)
+        {
+            line = sr.ReadLine();       // Read each line
+            var values = line.Split(',');
+            var message = values[1];    
+            long timeSeconds = long.Parse(values[^1])+7200; //Plus 7200 to adjust timezone 
+            var timeStamp = DateTimeOffset.FromUnixTimeSeconds(timeSeconds).DateTime; //Convert to DateTime
+            string formattedTimeStamp = timeStamp.ToString("dd/MM/yy HH:mm:ss"); //Format timeStamp - used GPT for this
+
+            for (int i = 2; i < values.Length-1;i++) //Reading the message and adding possible commas
             {
-                // Read the stream as a string, and write the string to the console.
-                Console.WriteLine(sr.ReadToEnd());
-            }
-        }
+                 message += "," + values[i];
+            }    
+            message = message.Trim('"');            //Trimming quotations
+            Console.WriteLine(values[0] +" @ "+ formattedTimeStamp +": "+ message);    
+        }      
+    }
         catch (IOException e)
         {
             Console.WriteLine("The file could not be read:");
             Console.WriteLine(e.Message);
         }
-
-        //Console.WriteLine(DateTimeOffset())
-
-
 }
 
-if (args[0]=="write")
+
+
+if (args[0]=="cheep")
 {
-    //WRITE CHEEP
+    long unixTimestamp = (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds; //Used GPT for this
+
+    using (StreamWriter sw = File.AppendText(path))  
+        {
+            string message = args[1]; //Saving the message given as argument inside quotations
+            sw.WriteLine(Environment.UserName + ",\"" + message + "\"," + unixTimestamp);
+        }
 }
-
-
-
-// using System.Threading.Tasks.Dataflow;
-
-
-// if (args.Length < 3 || args[0] != "say")
-// {
-//     Console.WriteLine("Please enter a command in the form: say <message> <amount>");
-//     return;
-// }
-// if (args[0] == "say" && args.Length == 3)
-// {
-//     var amount = int.Parse(args[2]);
-//     var random = new Random();
-
-//     for (int i = 0; i < amount; i++)
-//     {
-//         Console.WriteLine(args[1]);
-//         var wait = random.Next(100, 1000);
-//         await Task.Delay(TimeSpan.FromMilliseconds(wait));
-//     }
-// }
