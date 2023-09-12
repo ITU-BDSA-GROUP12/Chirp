987edc1489c0656d7ec805681209955557bdc379
Allan <asia@itu.dk>
2023-09-12 14:26:23 +0200

    Needs more work later

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -9,34 +9,31 @@ using SimpleDB;
 
 
 
+IDatabaseRepository<Cheep> data_access = new CSVDatabase<Cheep>("../src/chirp_cli_db.csv");
 
 
 if (args[0]=="read")
 {   //CSV Read part from: https://joshclose.github.io/CsvHelper/getting-started/
-    IDatabaseRepository<Cheep> data_access = new CSVDatabase<Cheep>("../src/chirp_cli_db.csv");
-    IEnumerable<Cheep> records = data_access.Read();
+    List<Cheep> records = data_access.Read();
     
-        if (args.Length == 1) 
+    if (args.Length == 1) 
+    {
+        foreach (Cheep cheep in records)
         {
-            foreach (Cheep cheep in records)
-            {
-                long timeSeconds = cheep.Timestamp + 7200; //Plus 7200 to adjust timezone 
-                var timeStamp = DateTimeOffset.FromUnixTimeSeconds(timeSeconds).DateTime; //Convert to DateTime
-                string formattedTimeStamp = timeStamp.ToString("dd/MM/yy HH:mm:ss"); //Format timeStamp - used GPT for this
-
-                Console.WriteLine($"{cheep.Author} @ {formattedTimeStamp} : {cheep.Message}");
-            }
+            long timeSeconds = cheep.Timestamp + 7200; //Plus 7200 to adjust timezone 
+            var timeStamp = DateTimeOffset.FromUnixTimeSeconds(timeSeconds).DateTime; //Convert to DateTime
+            string formattedTimeStamp = timeStamp.ToString("dd/MM/yy HH:mm:ss"); //Format timeStamp - used GPT for this
+            Console.WriteLine($"{cheep.Author} @ {formattedTimeStamp} : {cheep.Message}");
         }
-        else if (args.Length == 2)
+    }
+    else if (args.Length == 2)
+    {
+        int cheeps_left = int.Parse(args[1]);
+        foreach (Cheep cheep in records)
         {
-            int cheeps_left = int.Parse(args[1]);
-            foreach (Cheep cheep in records)
-
-            {
-                Console.WriteLine($"{cheep.Author} @ {cheep.Timestamp} : {cheep.Message}");
-                cheeps_left -= 1;
-                if (cheeps_left == 0) { break; }
-            }
+            Console.WriteLine($"{cheep.Author} @ {cheep.Timestamp} : {cheep.Message}");
+            cheeps_left -= 1;
+            if (cheeps_left == 0) { break; }
         }
     }
 }
@@ -45,11 +42,7 @@ if (args[0]=="cheep")
 {   //CSV Write part from: https://joshclose.github.io/CsvHelper/getting-started/
     long unixTimestamp = (long)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds; //Used GPT for this
     Cheep cheep = new Cheep(Environment.UserName , args[1] , unixTimestamp);
-    using (StreamWriter sw = File.AppendText(path))  
-    using (CsvWriter csv = new CsvWriter(sw , CultureInfo.InvariantCulture))
-    {
-        csv.WriteRecord(cheep);
-    }
+    data_access.Store(cheep);
 }
 
 
