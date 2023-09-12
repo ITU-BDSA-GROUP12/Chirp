6239ccedd08cbe0ad8f3fefcbda5bb6d9366c6da
Rasmus R <rhra@itu.dk>
2023-09-12 16:46:49 +0200

    install system command line package Co-authored-by: Markus: <mbrh@itu.dk> Co-authored-by: Lukas: <lupa@itu.dk> Co-authored-by: Rasmus: <rhra@itu.dk> Co-authored-by: Allan: <asia@itu.dk> Co-authored-by: Mads: <mhve@itu.dk>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -5,6 +5,7 @@ using System.Globalization;
 using System.ComponentModel.Design;
 using System.CommandLine;
 using System.Runtime.CompilerServices;
+using SimpleDB;
 
 //All references to "GPT" in comments are references to chat.opanai.com
 
@@ -28,21 +29,22 @@ class Program
         rootCommand.Add(readCommand);
         readCommand.Add(limitOption);
 
-        readCommand.SetHandler((limitOption)=>{
+        readCommand.SetHandler((limitOption) =>
+        {
             //CSV Read part from: https://joshclose.github.io/CsvHelper/getting-started/
-                IDatabaseRepository<Cheep> data_access = new CSVDatabase<Cheep>("../src/chirp_cli_db.csv");
-                IEnumerable<Cheep> records = data_access.Read();
-                
-                foreach (Cheep cheep in records)
-                {
-                    long timeSeconds = cheep.Timestamp + 7200; //Plus 7200 to adjust timezone 
-                    var timeStamp = DateTimeOffset.FromUnixTimeSeconds(timeSeconds).DateTime; //Convert to DateTime
-                    string formattedTimeStamp = timeStamp.ToString("dd/MM/yy HH:mm:ss"); //Format timeStamp - used GPT for this
+            IDatabaseRepository<Cheep> data_access = new CSVDatabase<Cheep>("../src/chirp_cli_db.csv");
+            IEnumerable<Cheep> records = data_access.Read();
 
-                    Console.WriteLine($"{cheep.Author} @ {formattedTimeStamp} : {cheep.Message}");
-                }
-            
-        },limitOption);
+            foreach (Cheep cheep in records)
+            {
+                long timeSeconds = cheep.Timestamp + 7200; //Plus 7200 to adjust timezone 
+                var timeStamp = DateTimeOffset.FromUnixTimeSeconds(timeSeconds).DateTime; //Convert to DateTime
+                string formattedTimeStamp = timeStamp.ToString("dd/MM/yy HH:mm:ss"); //Format timeStamp - used GPT for this
+
+                Console.WriteLine($"{cheep.Author} @ {formattedTimeStamp} : {cheep.Message}");
+            }
+
+        }, limitOption);
 
         /*
 
@@ -83,21 +85,21 @@ class Program
                     data_access.Store(cheep);
         }*/
 
-        if (args[0]=="cheep")
+        if (args[0] == "cheep")
         {   //CSV Write part from: https://joshclose.github.io/CsvHelper/getting-started/
             long unixTimestamp = (long)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds; //Used GPT for this
-            Cheep cheep = new Cheep(Environment.UserName , args[1] , unixTimestamp);
-            using (StreamWriter sw = File.AppendText(path))  
-            using (CsvWriter csv = new CsvWriter(sw , CultureInfo.InvariantCulture))
+            Cheep cheep = new Cheep(Environment.UserName, args[1], unixTimestamp);
+            using (StreamWriter sw = File.AppendText(path))
+            using (CsvWriter csv = new CsvWriter(sw, CultureInfo.InvariantCulture))
             {
                 csv.WriteRecord(cheep);
             }
         }
-    await rootCommand.InvokeAsync(args);
-    
+        await rootCommand.InvokeAsync(args);
 
-}
 
-    public record Cheep(string Author , string Message , long Timestamp);
+    }
+
+    public record Cheep(string Author, string Message, long Timestamp);
 
 }
