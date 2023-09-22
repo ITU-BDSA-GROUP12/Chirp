641dee2dbcb2ca918ca3369798dfe9eeb59fe0e5
Mads Hvelplund <mhve@itu.dk>
2023-09-22 21:07:07 +0200

    created PrepareCsvFile method
    if CSVDatabase.path does not point to a file, one is now created and
    the record properties are written as the first line

--- src/Chirp.CSVDB/CSVDatabase.cs
+++ src/Chirp.CSVDB/CSVDatabase.cs
@@ -1,4 +1,6 @@
 namespace SimpleDB;
+
+using System.ComponentModel;
 using System.Globalization;
 using CsvHelper;
 
@@ -8,7 +10,7 @@ public sealed class CSVDatabase<T> : IDatabaseRepository<T>
     private static CSVDatabase<T> instance = null;
     private static readonly object padlock = new object();
     string path;
-    private CSVDatabase(string path = "../src/chirp_cli_db.csv")
+    private CSVDatabase(string path = "../../src/chrip_cli_db.csv")
     {
         this.path = path;
     }
@@ -20,26 +22,36 @@ public sealed class CSVDatabase<T> : IDatabaseRepository<T>
         {
             throw new ArgumentOutOfRangeException($"{nameof(limit)} must be greater than 0");
         }
-        using (StreamReader reader = new StreamReader(path))
-        using (CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        if (Path.Exists(path))
         {
-            foreach (T record in csv.GetRecords<T>())
+            using (StreamReader reader = new StreamReader(path))
+            using (CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture))
             {
-                yield return record;
-                limit = limit == null ? null : limit - 1;
-                if (limit == 0) { break; }
+                foreach (T record in csv.GetRecords<T>())
+                {
+                    yield return record;
+                    limit = limit == null ? null : limit - 1;
+                    if (limit == 0) { break; }
+                }
             }
+        }
+        else
+        {
+            Console.WriteLine($"There are no cheeps to read from here: {path}\nPlease specify a different path, or simply begin cheeping to create some cheeps!");
         }
     }
     //CSV Write part from: https://joshclose.github.io/CsvHelper/getting-started/
     public void Store(T record)
     {
+        if (!Path.Exists(path))
+        {
+            PrepareCsvFile(record);
+        }
         using (StreamWriter sw = File.AppendText(path))
         using (CsvWriter csv = new CsvWriter(sw, CultureInfo.InvariantCulture))
         {
             csv.NextRecord();
             csv.WriteRecord(record);
-
         }
 
     }
@@ -58,6 +70,22 @@ public sealed class CSVDatabase<T> : IDatabaseRepository<T>
                 return instance;
             }
         }
+    }
+    private void PrepareCsvFile(T record)
+    {
+        string record_string = record.ToString(); // record_string looks something like this: Record{ Property1=Value1, Property2=Value2 ... }
+        string substring = record_string.Split('{')[1]; // we are only interested in the property names Property1 Property2 and so on
+        string[] substrings = substring.Split(',');
+        for (int i = 0 ; i < substrings.Length ; i++)
+        {
+            substrings[i] = substrings[i].Split('=')[0].Trim();
+        }
+        // substrings is now full of only the property string names
+        using (StreamWriter sw = File.AppendText(path)) // this creates the .csv file
+        {
+            sw.Write(string.Join("," , substrings)); // this writes in the property names of the record like so: Property1,Property2,...
+        }
+        // the file is now ready to be written to
     }
 }
 
