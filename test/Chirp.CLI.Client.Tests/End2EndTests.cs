2336abefcc3043acd40d95e25be446e8f653d26a
Mads Hvelplund <mhve@itu.dk>
2023-10-03 11:08:20 +0200

    the End2EndTest now passes
    Co-authored-by: Markus: <mbrh@itu.dk>
    Co-authored-by: Lukas: <lupa@itu.dk>
    Co-authored-by: Allan: <asia@itu.dk

--- test/Chirp.CLI.Client.Tests/End2EndTests.cs
+++ test/Chirp.CLI.Client.Tests/End2EndTests.cs
@@ -10,49 +10,20 @@ using System.Runtime.InteropServices;
 public class End2End
 {
 
-
     [Fact]
-    public void Test_That_A_Cheep_Is_Stored_As_Expected() //E2E test to test the cheep command.
+    public void Test_Read_And_Write_Cheep_1_Cheep() //E2E test of read --limit 1 and cheep
     {
         // Arrange
-        // Act
         using (var process = new Process())
         {
             process.StartInfo.FileName = dotNetPath();
-            process.StartInfo.Arguments = "bin/Debug/net7.0/Chirp.CLI.dll cheep \"this is a cheep for testing E2E cheeping\""; //The cheep msg
+            process.StartInfo.Arguments = "bin/Debug/net7.0/Chirp.CLI.dll cheep \"this is a cheep for endtoend test\""; //The cheep msg
             process.StartInfo.UseShellExecute = false;
             process.StartInfo.WorkingDirectory = "../../../../../src/Chirp.CLI.Client/";
             process.StartInfo.RedirectStandardOutput = true;
             process.Start();
             // Synchronously read the standard output of the spawned process.
             process.WaitForExit();
-        }
-
-        string output = "";
-        List<Cheep> cheepRecords = new List<Cheep>();
-        using (StreamReader reader = new StreamReader("../../../../../src/Chirp.CSVDBService/chirp_cli_db.csv"))
-        using (CsvReader csvReader = new CsvReader(reader, new CsvHelper.Configuration.CsvConfiguration(CultureInfo.InvariantCulture)))
-        {
-            cheepRecords = csvReader.GetRecords<Cheep>().ToList();
-            output = UserInterface.GetOutputString(cheepRecords[cheepRecords.Count()-1]); //Read the last record out into output string.
-        }
-
-        // Assert
-        System.Console.WriteLine(output);
-        Assert.EndsWith("this is a cheep for testing E2E cheeping", output.Trim()); //Trim to ensure that trailing whitespace does not interfere with the test.
-
-        RemoveLastCheep(); //Cleanup step
-    }
-
-    [Fact]
-    public void Test_Read_Cheep_Limit_1() //E2E test to test the read command
-    {
-        // Arrange
-        using (StreamWriter writer = File.AppendText("../../../../../src/Chirp.CSVDBService/chirp_cli_db.csv"))
-        using (CsvWriter csv = new CsvWriter(writer , CultureInfo.InvariantCulture))
-        {
-            csv.NextRecord();
-            csv.WriteRecord(new Cheep("allan","this is a cheep for testing E2E reading",1694520339)); //A cheep is added to the csv file, to be read in the test.
         }
         // Act
         string output = "";
@@ -70,40 +41,9 @@ public class End2End
             process.WaitForExit();
         }
         // Assert
-        Assert.StartsWith("allan" , output.Trim()); //Trim to ensure that trailing whitespace doesnt interfere with the test.
-        Assert.EndsWith("this is a cheep for testing E2E reading" , output.Trim());
-
-        RemoveLastCheep(); //Cleanup step
+        Assert.EndsWith("this is a cheep for endtoend test" , output.Trim());
     }
 
-    //A cleanup step that removes the cheep created by the tests.
-    private void RemoveLastCheep() //This method is written by chat.openai.com
-    {
-        string csvFilePath = "../../../../../src/Chirp.CSVDBService/chirp_cli_db.csv";
-
-        // Read all records from the CSV file
-        List<Cheep> cheepRecords = new List<Cheep>();
-        using (StreamReader reader = new StreamReader(csvFilePath))
-        using (CsvReader csvReader = new CsvReader(reader, new CsvHelper.Configuration.CsvConfiguration(CultureInfo.InvariantCulture)))
-        {
-            cheepRecords = csvReader.GetRecords<Cheep>().ToList();
-        }
-
-        // Check if there are records to delete
-        if (cheepRecords.Count > 0)
-        {
-            // Remove the last cheep record
-            cheepRecords.RemoveAt(cheepRecords.Count - 1);
-
-            // Write the modified data back to the CSV file
-            using (StreamWriter writer = new StreamWriter(csvFilePath))
-            using (CsvWriter csvWriter = new CsvWriter(writer, new CsvHelper.Configuration.CsvConfiguration(CultureInfo.InvariantCulture)))
-            {
-                csvWriter.WriteRecords(cheepRecords);
-            }
-        }
-        
-    }
 
     //Generate path for dotnetcore based on platform
     private string dotNetPath()
