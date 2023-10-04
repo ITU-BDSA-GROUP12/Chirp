132aa73b93f9031625bcc012a14dace7ec36bb94
Lukas <lupa@itu.dk>
2023-10-04 20:42:36 +0200

    Create test suite and implement initial testing

--- src/Chirp.Razor/CheepService.cs
+++ src/Chirp.Razor/CheepService.cs
@@ -95,7 +95,8 @@ public class CheepService : ICheepService
     private static string SeedingDBfileDir (){ // Retreeves the the given file from the EnvironmentVariable, else creates a path to the Tmp folder
         string chirpDBpath;                     // https://learn.microsoft.com/en-us/dotnet/api/system.environment.setenvironmentvariable?view=net-7.0
         if(Environment.GetEnvironmentVariable("CHIRPDBPATH") == null){
-            chirpDBpath = Path.GetTempPath() + "/chirp.db";
+            // chirpDBpath = Path.GetTempPath() + "/chirp.db";
+            chirpDBpath = "/tmp/chirp.db"; //For now this has to be set for test to run. When merged with new create db feate, we can change back to above
         } else {
             chirpDBpath = Environment.GetEnvironmentVariable("CHIRPDBPATH");
         }
---
+++ test/Chirp.Razor.Tests/IntegrationTest.cs
@@ -0,0 +1,42 @@
+namespace Chirp.Razor.Tests;
+
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+
+public class IntegrationTest : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _fixture;
+    private readonly HttpClient _client;
+
+    public TestAPI(WebApplicationFactory<Program> fixture)
+    {
+        _fixture = fixture;
+        _client = _fixture.CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = true, HandleCookies = true });
+    }
+
+    [Fact]
+    public async void CanSeePublicTimeline()
+    {
+        var response = await _client.GetAsync("/");
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+
+        Assert.Contains("Chirp!", content);
+        Assert.Contains("Public Timeline", content);
+    }
+
+    [Theory]
+    [InlineData("Helge")]
+    [InlineData("Rasmus")]
+    public async void CanSeePrivateTimeline(string author)
+    {
+        var response = await _client.GetAsync($"/{author}");
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+
+        Assert.Contains("Chirp!", content);
+        Assert.Contains($"{author}'s Timeline", content);
+    }
+}
