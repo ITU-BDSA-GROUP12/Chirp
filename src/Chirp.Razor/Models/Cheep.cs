a09ea0c9081d796408ea02d2b17afe26c6646760
Markus Brandt Højgaard <mbrh@itu.dk>
2023-10-10 15:18:34 +0200

    Created DTO and refactor pages
    Co-authored-by: Allan: <asia@itu.dk>
    Co-authored-by: Lukas: <lupa@itu.dk>

--- src/Chirp.Razor/Models/Cheep.cs
+++ src/Chirp.Razor/Models/Cheep.cs
@@ -4,6 +4,12 @@ public class Cheep
 {
     public int CheepId { get; set; }
     public Author Author { get; set; }
-    public string Text { get; set; }
-    public DateTime TimeStamp { get; set; }
+    public string Message { get; set; }
+    public DateTime Timestamp { get; set; }
+}
+
+public class CheepDto { //data transfer object (DTO) https://learn.microsoft.com/en-us/aspnet/web-api/overview/data/using-web-api-with-entity-framework/part-5
+    public Author Author { get; set; }
+    public string Message { get; set; }
+    public string Timestamp { get; set; }
 }
--- src/Chirp.Razor/Pages/Public.cshtml.cs
+++ src/Chirp.Razor/Pages/Public.cshtml.cs
@@ -6,20 +6,20 @@ namespace Chirp.Razor.Pages;
 public class PublicModel : PageModel
 {
     private readonly ICheepRepository _repository;
-    public List<CheepViewModel> Cheeps { get; set; }
+    public List<CheepDto> Cheeps { get; set; }
 
     public PublicModel(ICheepRepository repository)
     {
         _repository = repository;
     }
 
-    public ActionResult OnGet()
+    public async Task<IActionResult> OnGet() //use of Task https://learn.microsoft.com/en-us/aspnet/core/data/ef-rp/crud?view=aspnetcore-7.0
     {
         string? pagevalue = Request.Query["page"];
         if(pagevalue == null){
-            Cheeps = _repository.GetCheeps(1);
+            Cheeps = await _repository.GetCheeps(1);
         }else {
-            Cheeps = _repository.GetCheeps(Int32.Parse(pagevalue));
+            Cheeps = await _repository.GetCheeps(Int32.Parse(pagevalue));
         }
         return Page();
     }
