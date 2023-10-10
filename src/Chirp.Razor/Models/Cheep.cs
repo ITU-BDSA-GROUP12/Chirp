efab45b2424f9ab8731ddd50c11259d3f8b62e8b
Markus Brandt Højgaard <mbrh@itu.dk>
2023-10-10 12:07:30 +0200

    The Author model class
    Co-authored-by: Allan: <asia@itu.dk>
    Co-authored-by: Lukas: <lupa@itu.dk>

--- src/Chirp.Razor/Models/Cheep.cs
+++ src/Chirp.Razor/Models/Cheep.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 public class Cheep
 {
     public int CheepId { get; set; }
-    public Aurthor Aurthor { get; set; }
+    public Author Author { get; set; }
     public string Text { get; set; }
     public DateTime TimeStamp { get; set; }
 }
