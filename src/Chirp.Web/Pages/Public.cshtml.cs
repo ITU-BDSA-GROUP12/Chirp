2eac093ba5ea630a54e2df7801c48e3e032ea59e
Allan <asia@itu.dk>
2023-10-24 11:07:17 +0200

    add chirp.web - Runs
    Longer description...
    
    Co-authored-by: Markus: <mbrh@itu.dk>
    Co-authored-by: Lukas: <lupa@itu.dk>
    Co-authored-by: Mads: <mhve@itu.dk>

---
+++ src/Chirp.Web/Pages/Public.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chirp.Web.Pages;
+
+public class PublicModel : PageModel
+{
+    private readonly ICheepRepository _repository;
+    public List<CheepDto> Cheeps { get; set; }
+
+    public PublicModel(ICheepRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<IActionResult> OnGet() //use of Task https://learn.microsoft.com/en-us/aspnet/core/data/ef-rp/crud?view=aspnetcore-7.0
+    {
+        string? pagevalue = Request.Query["page"];
+        if(pagevalue == null){
+            Cheeps = await _repository.GetCheeps(1);
+        }else {
+            Cheeps = await _repository.GetCheeps(Int32.Parse(pagevalue));
+        }
+        return Page();
+    }
+}
---
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chirp.Web.Pages;
+
+public class UserTimelineModel : PageModel
+{
+    private readonly ICheepRepository _repository;
+    public List<CheepDto> Cheeps { get; set; }
+
+    public UserTimelineModel(ICheepRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<IActionResult> OnGet(string author)
+    {
+        string? pagevalue = Request.Query["page"];
+        if(pagevalue == null){
+            Cheeps = await _repository.GetCheepsFromAuthor(1,author);
+        }else {
+            Cheeps = await _repository.GetCheepsFromAuthor(Int32.Parse(pagevalue), author);
+        }
+        return Page();
+    }
+}
