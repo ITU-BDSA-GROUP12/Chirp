3c9967045ca78783d4331d00c66124d08b2a7792
Mads Hvelplund <mhve@itu.dk>
2023-10-24 15:29:06 +0200

    now using guid instead of int
    Co-authored-by: Markus: <mbrh@itu.dk>
    Co-authored-by: Lukas: <lupa@itu.dk>
    Co-authored-by: Allan: <asia@itu.dk>

--- src/Chirp.Infrastructure/Entities/Cheep.cs
+++ src/Chirp.Infrastructure/Entities/Cheep.cs
@@ -2,8 +2,8 @@ using System.ComponentModel.DataAnnotations;
 
 public class Cheep
 {
-    public int CheepId { get; set; }
-    public int AuthorId { get; set; }
+    public Guid CheepId { get; set; }
+    public Guid AuthorId { get; set; }
     public Author Author { get; set; }
     public string Text { get; set; }
     public DateTime TimeStamp { get; set; }
--- src/Chirp.Infrastructure/data/ChirpDBContext.cs
+++ src/Chirp.Infrastructure/data/ChirpDBContext.cs
@@ -1,4 +1,6 @@
 
+global using Microsoft.EntityFrameworkCore;
+namespace Chirp.Infrastructure;
     public class ChirpDBContext : DbContext
     {
         
@@ -7,7 +9,17 @@
         {
         }
 
-        public DbSet<Author> Authors { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>()
+            .HasIndex(a => a.AuthorId)
+            .IsUnique();
+        modelBuilder.Entity<Cheep>()
+            .HasIndex(c => c.CheepId)
+            .IsUnique();
+    }
+
+    public DbSet<Author> Authors { get; set; }
         public DbSet<Cheep> Cheeps { get; set; }
     }
 
