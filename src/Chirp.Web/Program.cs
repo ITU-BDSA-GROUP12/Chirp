c480ce28789588e6349007debf614d374e29c8b6
Markus Brandt Højgaard <mbrh@itu.dk>
2023-11-09 13:16:08 +0100

    Only use user-secrets in dev

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -9,14 +9,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Configuration.AddJsonFile("appsettings.json");
-builder.Configuration.AddUserSecrets<Program>();
-
-/*var tempFolder = Path.GetTempPath();
-var dbFileName = "chirp.db";
-var dbPath = Path.Combine(tempFolder, dbFileName);
-builder.Configuration["ConnectionStrings:ChirpDbConnectionSQlite"] = $"Data Source={dbPath}";
-
-var connString = builder.Configuration.GetConnectionString("ChirpDbConnectionSQlite");*/
 
 // Add services to the container.
 builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
@@ -33,6 +25,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    builder.Configuration.AddUserSecrets<Program>();
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
