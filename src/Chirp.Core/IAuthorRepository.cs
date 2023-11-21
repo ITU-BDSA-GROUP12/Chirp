a3a05dbe5d2aef94aafac5ec68407da14991f42f
Markus Brandt Højgaard <mbrh@itu.dk>
2023-11-21 14:23:52 +0100

    minor fix

--- src/Chirp.Core/IAuthorRepository.cs
+++ src/Chirp.Core/IAuthorRepository.cs
@@ -5,4 +5,6 @@ public interface IAuthorRepository
     public Task<AuthorDto?> GetAuthorDTOByName(string name);
     public Task<AuthorDto?> GetAuthorDTOByEmail(string email);
     public Task CreateAuthor(string name, string email);
+    public Task FollowAnAuthor(string followingEmail, string followedEmail);
+    public Task UnFollowAnAuthor(string followingEmail, string unFollowingEmail);
 }
