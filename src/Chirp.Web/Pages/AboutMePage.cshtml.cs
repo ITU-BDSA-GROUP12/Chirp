2aafabc8e21abbebba380c22dbfaff3c4e9cce46
Rasmus R <rhra@itu.dk>
2023-11-28 11:10:01 +0100

    we are just missing getAuthorById, but need to touch the repository for that

--- src/Chirp.Web/Pages/AboutMePage.cshtml.cs
+++ src/Chirp.Web/Pages/AboutMePage.cshtml.cs
@@ -9,8 +9,9 @@ public class AboutMePageModel : PageModel
     public ICheepRepository _cheepRepository;
     public IAuthorRepository _authorRepository;
     public List<CheepDto>? Cheeps { get; set; }
+    public List<Guid>? Followers { get; set; }
     public string? author { get; set; }
-    public string? emails { get; set; } 
+    public string? email { get; set; } 
 
 
     public AboutMePageModel(ICheepRepository cheepRepository, IAuthorRepository authorRepository)
@@ -20,9 +21,22 @@ public class AboutMePageModel : PageModel
     }
 
     public async Task<IActionResult> OnGet()
-    {   
+    {  
+        string? pagevalue = Request.Query["page"];
+    
         author = User.Identity.Name;
-        emails = User.FindFirstValue("emails");
+        email = User.FindFirstValue("emails");
+            if (pagevalue == null)
+            {
+                Cheeps = await _cheepRepository.GetCheepsFromAuthor(1, author);
+            }
+            else
+            {
+                Cheeps = await _cheepRepository.GetCheepsFromAuthor(Int32.Parse(pagevalue), author);
+            }
+        
+        Followers = await _authorRepository.GetFollowedAuthors(email);
+        
         return Page();
     }
 }
