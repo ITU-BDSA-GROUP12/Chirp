50b3aa5a25643799690a2df1c23cf650d968efc6
Markus Brandt Højgaard <mbrh@itu.dk>
2023-12-04 07:13:19 +0100

    almoste there
    There is an error stating that user with my email is not in the context

--- src/Chirp.Web/Pages/AboutMePage.cshtml.cs
+++ src/Chirp.Web/Pages/AboutMePage.cshtml.cs
@@ -1,6 +1,12 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
+using System.Linq;
+using System;
+using System.Threading.Tasks;
+using Azure.Identity;
 
 namespace Chirp.Web.Pages;
 
@@ -11,8 +17,9 @@ public class AboutMePageModel : PageModel
     public List<CheepDto>? Cheeps { get; set; }
     public List<Guid>? FollowersID { get; set; }
     public List<string>? FollowersName { get; set; }
-    public string? author { get; set; }
-    public string? email { get; set; } 
+    public string? Author { get; set; }
+    public string? Email { get; set; }
+
 
 
     public AboutMePageModel(ICheepRepository cheepRepository, IAuthorRepository authorRepository)
@@ -22,43 +29,94 @@ public class AboutMePageModel : PageModel
     }
 
     public async Task<IActionResult> OnGet()
-    {  
+    {
         string? pagevalue = Request.Query["page"];
-    
-        author = User.Identity.Name;
-        email = User.FindFirstValue("emails");
-            if (pagevalue == null)
-            {
-                Cheeps = await _cheepRepository.GetCheepsFromAuthor(1, author);
-            }
-            else
-            {
-                Cheeps = await _cheepRepository.GetCheepsFromAuthor(Int32.Parse(pagevalue), author);
-            }
-        
-        FollowersID = await _authorRepository.GetFollowedAuthors(email);
-        
+
+        Author = User.Identity.Name;
+        Email = User.FindFirstValue("emails");
+        Console.WriteLine(Email);
+        if (pagevalue == null)
+        {
+            Cheeps = await _cheepRepository.GetCheepsFromAuthor(1, Author);
+        }
+        else
+        {
+            Cheeps = await _cheepRepository.GetCheepsFromAuthor(Int32.Parse(pagevalue), Author);
+        }
+
+        FollowersID = await _authorRepository.GetFollowedAuthors(Email);
+
         FollowersName = new List<string>();
         foreach (Guid id in FollowersID)
         {
-    
+
             string user = await _authorRepository.GetAuthorNameByID(id);
             if (user != null)
             {
-                FollowersName.Add(user);   
+                FollowersName.Add(user);
             }
         }
 
-        
+
         return Page();
     }
 
-    public async Task<IActionResult> OnPostDelete(string email)
+     public async Task<IActionResult> OnPostDelete(string UserEmail)
         {
-            await _authorRepository.DeleteAuthor(email);
+            try
+            {
+                var clientId = "e122fcdf-99a1-4b19-b7a4-adf859e617ca";
+                var tenantId = "ab2f43aa-cecc-43ed-a142-34012b9a7a3b";
+                var clientSecret = "Client_Secret";
+
+                var scopes = new[] { "https://graph.microsoft.com/.default"};
+
+                var options = new TokenCredentialOptions
+                {
+                    AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
+                };
+
+                var clientSecretCredential = new ClientSecretCredential(
+                    tenantId, clientId, clientSecret, options);
+
+                var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
+
+                // Get User ID by Email
+                string userId = await GetUserIdByEmailAsync(graphClient, UserEmail);
+
+                // Delete User by ID
+                await DeleteUserAsync(graphClient, UserEmail);
+
+                Console.WriteLine($"User with email {UserEmail} deleted successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error deleting user with email: {UserEmail}, {ex.Message}");
+            }
 
             string redirectUrl = "~/";
-
             return Redirect(Url.Content(redirectUrl));
         }
+
+    private async Task<string> GetUserIdByEmailAsync(GraphServiceClient graphClient, string UserEmail)
+    {
+         var user = await graphClient
+            .Users[UserEmail]
+            .GetAsync();
+
+        Console.WriteLine(user.Id);
+        return user?.Id;
+    }
+
+    private async Task DeleteUserAsync(GraphServiceClient graphClient, string userId)
+    {
+        if (userId != null)
+        {
+            await graphClient.Users[userId].DeleteAsync();
+        }
+        else
+        {
+            throw new InvalidOperationException("User ID not found for the given email.");
+        }
+    }
 }
