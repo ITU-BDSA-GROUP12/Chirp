efd70c256b080f7eafa1fb67ed27f9c8ccdd86a0
Rasmus R <rhra@itu.dk>
2023-12-07 12:32:24 +0100

    Do the thing were poops are not allowed to cheep pls

--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -54,6 +54,7 @@ public class UserTimelineModel : PageModel
         if (String.IsNullOrEmpty(Text)) {
             validationMessage = "Cheep cannot be empty";
             return OnGet(User.Identity.Name).Result;
+            //lol
         }
 
         if (Text.Length > 160)
