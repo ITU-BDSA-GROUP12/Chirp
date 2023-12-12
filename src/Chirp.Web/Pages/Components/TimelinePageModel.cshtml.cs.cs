9602613073542d2869b89d4f2f87e2c11154fbf9
Lukas <lupa@itu.dk>
2023-12-12 16:25:55 +0100

    New file for mads stuff
    Co-authored-by: Mads: <mhve@itu.dk>
    Co-authored-by: Markus: <mbrh@itu.dk>

---
+++ src/Chirp.Web/Pages/Components/TimelinePageModel.cshtml.cs.cs
@@ -0,0 +1,75 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chirp.Web.Pages.Components;
+
+public class TimelinePageModel : PageModel
+{
+
+    public ICheepRepository _CheepRepository;
+    public IAuthorRepository _AuthorRepository;
+
+    public string validationMessage { get; set; } = "";
+
+
+    public TimelinePageModel(ICheepRepository repository, IAuthorRepository authorRepository)
+    {
+        _CheepRepository = repository;
+        _AuthorRepository = authorRepository;
+    }
+
+    [BindProperty]
+    public string Text { get; set; }
+
+    public async Task<IActionResult> OnPost()
+    {
+        string redirectUrl = "~/";
+
+        validationMessage = "";
+        if (String.IsNullOrEmpty(Text))
+        {
+            validationMessage = "Cheep cannot be empty";
+            return Redirect(Url.Content(redirectUrl));
+        }
+
+        if (Text.Length > 160)
+        {
+            validationMessage = "Cheep cannot be longer than 160 characters";
+            return Redirect(Url.Content(redirectUrl));
+        }
+
+
+        AuthorDto author = new()
+        {
+            Name = User.Identity.Name,
+            Email = User.FindFirstValue("emails")// from https://stackoverflow.com/questions/30701006/how-to-get-the-current-logged-in-user-id-in-asp-net-core
+        };
+        await _CheepRepository.CreateCheep(Text, author);
+
+
+        return Redirect(Url.Content(redirectUrl));
+    }
+
+     //https://www.learnrazorpages.com/razor-pages/handler-methods
+    public async Task<IActionResult> OnPostFollow(string followName)
+    {
+        string emailOfUserThatWantsToFollow = User.FindFirstValue("emails") ?? "";
+        await _AuthorRepository.FollowAnAuthor(emailOfUserThatWantsToFollow, followName);
+
+        string redirectUrl = "~/";
+
+        return Redirect(Url.Content(redirectUrl));
+    }
+
+    public async Task<IActionResult> OnPostUnFollow(string followName)
+    {
+        string emailOfUserThatWantsToUnfollow = User.FindFirstValue("emails") ?? "";
+        await _AuthorRepository.UnFollowAnAuthor(emailOfUserThatWantsToUnfollow, followName);
+
+
+        string redirectUrl = "~/";
+
+        return Redirect(Url.Content(redirectUrl));
+    }
+}
--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Web.Pages;
 
-public class UserTimelineModel : PageModel
+public class UserTimelineModel : Components.TimelinePageModel
 {
     public ICheepRepository _cheepRepository;
     public IAuthorRepository _authorRepository;
@@ -15,9 +15,8 @@ public class UserTimelineModel : PageModel
 
     public List<Guid>? FollowedAuthors { get; set; }
 
-    public string validationMessage { get; set; } = "";
 
-    public UserTimelineModel(ICheepRepository cheepRepository, IAuthorRepository authorRepository)
+    public UserTimelineModel(ICheepRepository cheepRepository, IAuthorRepository authorRepository) : base(cheepRepository, authorRepository)
     {
         _cheepRepository = cheepRepository;
         _authorRepository = authorRepository;
@@ -43,61 +42,6 @@ public class UserTimelineModel : PageModel
         HasNextPage = await _cheepRepository.HasNextPageOfPrivateTimeline(PageNumber, author, FollowedAuthors);
 
         return Page();
-    }
-
-    [BindProperty]
-    public string Text { get; set; }
-
-    public async Task<IActionResult> OnPost()
-    {
-        string redirectUrl = "~/";
-
-        validationMessage = "";
-        if (String.IsNullOrEmpty(Text))
-        {
-            validationMessage = "Cheep cannot be empty";
-            return OnGet(User.Identity.Name).Result;
-        }
-
-        if (Text.Length > 160)
-        {
-            validationMessage = "Cheep cannot be longer than 160 characters";
-            return OnGet(User.Identity.Name).Result;
-        }
-
-        if (User.Identity == null) throw new Exception("User attempted to send cheep without being logged in");
-        string? userName = User.Identity.Name;
-        string? userEmail = User.FindFirstValue("emails");
-        if (userName == null) throw new Exception("User has no username");
-        if (userEmail == null) throw new Exception("User has not registrered an email");
-
-        AuthorDto author = new()
-        {
-            Name = userName,
-            Email = userEmail// from https://stackoverflow.com/questions/30701006/how-to-get-the-current-logged-in-user-id-in-asp-net-core
-        };
-        await _cheepRepository.CreateCheep(Text, author);
-
-        return Redirect(Url.Content(redirectUrl));
-    }
-    public async Task<IActionResult> OnPostFollow(string followName)
-    {
-        string emailOfUserThatWantsToFollow = User.FindFirstValue("emails") ?? "";
-        await _authorRepository.FollowAnAuthor(emailOfUserThatWantsToFollow, followName);
-
-        string redirectUrl = "~/";
-
-        return Redirect(Url.Content(redirectUrl));
-    }
-
-    public async Task<IActionResult> OnPostUnFollow(string followName)
-    {
-        string emailOfUserThatWantsToUnfollow = User.FindFirstValue("emails") ?? "";
-        await _authorRepository.UnFollowAnAuthor(emailOfUserThatWantsToUnfollow, followName);
-
-        string redirectUrl = "~/";
-
-        return Redirect(Url.Content(redirectUrl));
     }
 
 }
