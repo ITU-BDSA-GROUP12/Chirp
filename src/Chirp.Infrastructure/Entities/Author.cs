233925a18466aa254798c9840711e99582a7cd63
Allan Sigge <allan.sigge.andersen@gmail.com>
2023-12-18 17:18:28 +0100

    add cheep desc

--- src/Chirp.Infrastructure/Entities/Author.cs
+++ src/Chirp.Infrastructure/Entities/Author.cs
@@ -1,13 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Chirp.Infrastructure;
 /// <summary>
-/// This class represents a Chirp user. When a user is authenticated we save them as an Author.
+/// The Author entity class represents a Chirp user. When a user is authenticated we save them as an Author.
 /// An author has basic information ID (we use GUID), name & email.
 /// An author can _Cheep_, ie. send messages. An authors cheeps are saved here in the Cheeps list.
 /// Furthermore, authors can follow each other, which is why we need two lists for followers and the followed users.
 /// </summary>
 
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-namespace Chirp.Infrastructure;
 public class Author
 {
     public Guid AuthorId { get; set; }
--- src/Chirp.Infrastructure/Entities/Cheep.cs
+++ src/Chirp.Infrastructure/Entities/Cheep.cs
@@ -1,10 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+namespace Chirp.Infrastructure;
 /// <summary>
-/// 
+/// The Cheep entity class represents the message an author can post.
+/// A Cheep has knowledge about its author, being AuthorId and a reference to the Author itself. 
+/// The Cheep of course contains the message, which is the Text field. A timestamp for the message is also saved.
 /// </summary>
 
 
-using System.ComponentModel.DataAnnotations;
-namespace Chirp.Infrastructure;
 public class Cheep
 {
     public Guid CheepId { get; set; }
