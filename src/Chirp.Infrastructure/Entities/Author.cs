407861a5c8ad0b266a666275e7f6f27e4cdb77dc
Markus Brandt Højgaard <mbrh@itu.dk>
2023-12-19 10:55:28 +0100

    delete unused usings. update ID and secret.

--- src/Chirp.Infrastructure/Entities/Author.cs
+++ src/Chirp.Infrastructure/Entities/Author.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 namespace Chirp.Infrastructure;
 public class Author
 {
--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -1,6 +1,5 @@
 ﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Web.Pages;
 
