bfc726d31d9b971607a644238e2f9fd18a12a1fd
Allan <asia@itu.dk>
2023-12-19 08:27:34 +0100

    required

--- src/Chirp.Infrastructure/Entities/Author.cs
+++ src/Chirp.Infrastructure/Entities/Author.cs
@@ -4,8 +4,10 @@ namespace Chirp.Infrastructure;
 /// <summary>
 /// The Author entity class represents a Chirp user. When a user is authenticated we save them as an Author.
 /// An author has basic information ID (we use GUID), name & email.
-/// An author can _Cheep_, i.e. send messages. An author's cheeps are saved in 'Cheeps'.
-/// Furthermore, authors can follow each other, which is why we need two lists for followers and the followed users.
+/// When creating new Authors it is assigned a new GUID which is why the field does not need to be required.
+/// An author can Cheep, i.e. post messages. An author's cheeps are saved in 'Cheeps'.
+/// Authors can follow each other, why we need lists for followers and followed users.
+/// 
 /// Hvorfor fuck er ting required, nullable?? Warnings?
 /// </summary>
 
