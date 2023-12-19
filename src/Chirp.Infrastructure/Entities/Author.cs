6e3ae9703f6efff8df2b5513138e156a0ebf099f
Allan <asia@itu.dk>
2023-12-19 13:15:52 +0100

    jaja nu er den her

--- src/Chirp.Infrastructure/Entities/Author.cs
+++ src/Chirp.Infrastructure/Entities/Author.cs
@@ -4,11 +4,8 @@ namespace Chirp.Infrastructure;
 /// <summary>
 /// The Author entity class represents a Chirp user. When a user is authenticated we save them as an Author.
 /// An author has basic information ID (we use GUID), name & email.
-/// When creating new Authors it is assigned a new GUID which is why the field does not need to be required.
 /// An author can Cheep, i.e. post messages. An author's cheeps are saved in 'Cheeps'.
 /// Authors can follow each other, why we need lists for followers and followed users.
-/// 
-/// Hvorfor fuck er ting required, nullable?? Warnings?
 /// </summary>
 
 public class Author
--- src/Chirp.Infrastructure/Entities/Cheep.cs
+++ src/Chirp.Infrastructure/Entities/Cheep.cs
@@ -4,9 +4,7 @@ namespace Chirp.Infrastructure;
 /// The Cheep entity class represents the message an author can post.
 /// When posting/creating a new Cheep the CheepId is given and the AuthorId is saved.
 /// To save time when loading Cheeps a reference to the Author itself is saved with the Cheep.
-/// Since our code relies on this, we marked the author field as required to avoid runtime 
-/// exceptions if someone tried to break the code. Also to maintain databse integrity.
-/// The Cheep contains the message => Text field. This field needs to be required as it is a user input. 
+/// The Cheep contains the message => Text field.
 /// The Timestamp for when the message was send is also saved by the app server.
 /// </summary>
 
